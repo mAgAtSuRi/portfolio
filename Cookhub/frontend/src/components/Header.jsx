dc0b2fbe954f485916bce800dff36b39683182bb
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-02-28 14:28:37 +0100

    logo formatted

--- Cookhub/frontend/src/components/Header.jsx
+++ Cookhub/frontend/src/components/Header.jsx
@@ -5,14 +5,14 @@ function Header() {
         <header className="bg-white">
             <div className="px-4 flex justify-between items-center">
                 <Link to="/my-recipes">
-                    <img src="/cookhub_logo.png" alt="logo cookhub" className="h-16" />
+                    <img src="/cookhub_logo.png" alt="logo cookhub" className="h-24" />
                 </Link>
                 <nav className="flex gap-4">
                     <Link to="/my-recipes">
-                        <button className="btn btn-outline">My Recipes</button>
+                        <button className="btn btn-ghost">My Recipes</button>
                     </Link>
                     <Link to="/shopping-list">
-                        <button className="btn btn-outline">Shopping List</button>
+                        <button className="btn btn-ghost">Shopping List</button>
                     </Link>
                 </nav>
                 <Link to="/login">
