cdec526389ba234bdad38a7fab188f6e16ccdf1a
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-03-28 23:00:42 +0100

    allow all vercel domains

--- Cookhub/backend/app/main.py
+++ Cookhub/backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5500",
-        "https://portfolio-38dj.vercel.app",
+        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
